e1f95d72a862c98a13eba2ad969ac26b0351eb3e
Antoine Dubé <antoine.dube@gmail.com>
2022-09-29 15:25:36 -0400

    Use logger instead of print-to-terminal

--- scripts/buy-servers.js
+++ scripts/buy-servers.js
@@ -5,60 +5,57 @@ function myMoney(ns) {
 }
 
 export async function main(ns) {
-	const targetCount = 18;
-	const targetLevel = 100;
-	const targetRam = 32;
-	const targetCore = 32;
+    const targetCount = 18;
+    const targetLevel = 100;
+    const targetRam = 32;
+    const targetCore = 8;
 
-	while (ns.hacknet.numNodes() < targetCount) {
-		const cost = ns.hacknet.getPurchaseNodeCost();
-		while (myMoney(ns) < cost) {
-			ns.tprint("Need $" + cost + " . Have $" + myMoney(ns) + " to buy new node");
-			await ns.sleep(1000*60);
-		}
+    while (ns.hacknet.numNodes() < targetCount) {
+        const cost = ns.hacknet.getPurchaseNodeCost();
+        while (myMoney(ns) < cost) {
+            ns.print("Need $" + cost + " . Have $" + myMoney(ns) + " to buy new node");
+            await ns.sleep(1000*60);
+        }
 
-		ns.hacknet.purchaseNode();
-	};
+        ns.hacknet.purchaseNode();
+    };
 
-	for (let i = 0; i < targetCount; i++) {
-		while (ns.hacknet.getNodeStats(i).level < targetLevel) {
-			const cost = ns.hacknet.getLevelUpgradeCost(i, 1);
-			while (myMoney(ns) < cost) {
-				ns.tprint("Need $" + cost + " . Have $" + myMoney(ns) + " to upgrade level");
-				await ns.sleep(1000*60);
-			}
-			const res = ns.hacknet.upgradeLevel(i, 1);
-			ns.tprint("Upgraded level of node " + i + " with result: " + res);
-		};
-	};
+    for (let i = 0; i < targetCount; i++) {
+        while (ns.hacknet.getNodeStats(i).level < targetLevel) {
+            const cost = ns.hacknet.getLevelUpgradeCost(i, 1);
+            while (myMoney(ns) < cost) {
+                ns.print("Need $" + cost + " . Have $" + myMoney(ns) + " to upgrade level");
+                await ns.sleep(1000*60);
+            }
+            ns.hacknet.upgradeLevel(i, 1);
+        };
+    };
 
-	ns.tprint("All nodes upgraded to level " + targetLevel);
+    ns.print("All nodes upgraded to level " + targetLevel);
 
-	for (let i = 0; i < targetCount; i++) {
-		while (ns.hacknet.getNodeStats(i).ram < targetRam) {
-			const cost = ns.hacknet.getRamUpgradeCost(i, 1);
-			while (myMoney(ns) < cost) {
-				ns.tprint("Need $" + cost + " . Have $" + myMoney(ns) + " to upgrade ram");
-				await ns.sleep(1000*60);
-			}
-			const res = ns.hacknet.upgradeRam(i, 1);
-			ns.tprint("Upgraded ram of node " + i + " with result: " + res);
-		};
-	};
+    for (let i = 0; i < targetCount; i++) {
+        while (ns.hacknet.getNodeStats(i).ram < targetRam) {
+            const cost = ns.hacknet.getRamUpgradeCost(i, 1);
+            while (myMoney(ns) < cost) {
+                ns.print("Need $" + cost + " . Have $" + myMoney(ns) + " to upgrade ram");
+                await ns.sleep(1000*60);
+            }
+            ns.hacknet.upgradeRam(i, 1);
+        };
+    };
 
-	ns.tprint("All nodes upgraded to " + targetRam + "GB RAM");
+    ns.print("All nodes upgraded to " + targetRam + "GB RAM");
 
-	for (let i = 0; i < targetCount; i++) {
-		while (ns.hacknet.getNodeStats(i).cores < targetCore) {
-			const cost = ns.hacknet.getCoreUpgradeCost(i, 1);
-			while (myMoney(ns) < cost) {
-				ns.tprint("Need $" + cost + " . Have $" + myMoney(ns) + " to upgrade core");
-				await ns.sleep(1000*60);
-			}
-			const res = ns.hacknet.upgradeCore(i, 1);
-			ns.tprint("Upgraded core number of node " + i + " with result: " + res);
-		};
-	};
+    for (let i = 0; i < targetCount; i++) {
+        while (ns.hacknet.getNodeStats(i).cores < targetCore) {
+            const cost = ns.hacknet.getCoreUpgradeCost(i, 1);
+            while (myMoney(ns) < cost) {
+                ns.print("Need $" + cost + " . Have $" + myMoney(ns) + " to upgrade core");
+                await ns.sleep(1000*60);
+            }
+            ns.hacknet.upgradeCore(i, 1);
+        };
+    };
 
-	ns.tprint("All nodes upgraded to " + targetCore + " cores");
+    ns.print("All nodes upgraded to " + targetCore + " cores");
 }
