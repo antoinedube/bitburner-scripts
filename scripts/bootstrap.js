32b2893eb0a8360b4931a6d3775a60b4b4e5ab7e
Antoine Dubé <antoine.dube@gmail.com>
2023-03-13 07:45:05 -0400

    Export scripts

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -1,11 +1,25 @@
 /** @param {NS} ns */
 export async function main(ns) {
-	const scripts = ['launch-hacking.js', 'buy-hacknet-nodes.js', 
-					 'buy-servers.js' /*, 'build-hacking-programs.js'*/];
+	const scripts = ['launch-hacking.js', 'buy-hacknet-nodes.js', 'buy-servers.js'];
 
 	for (let script of scripts) {
 		if (!ns.isRunning(script)) {
 			ns.run(script);
 		}
 	}
+
+	await ns.sleep(5000);
+
+	const hackingScript = 'hack-remote.js';
+	const scriptRam = ns.getScriptRam(hackingScript);
+    const availableRam = ns.getServerMaxRam('home') - ns.getServerUsedRam('home');
+    ns.print(`Available ram: ${availableRam}`);
+
+    const scriptNumThreads = ~~(availableRam / scriptRam);
+
+    ns.print(`Script num threads: ${scriptNumThreads}`);
+
+    if (scriptNumThreads>0) {
+        ns.exec(hackingScript, 'home', scriptNumThreads);
+    }
 }
