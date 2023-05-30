8730be15f8aea8da421c956144d95f0a68d880db
Antoine Dubé <antoine.dube@gmail.com>
2023-05-30 08:18:16 -0400

    Clean-up

--- scripts/launch-on-servers.js
+++ scripts/launch-on-servers.js
@@ -2,27 +2,27 @@ import { scan } from "./scan.js";
 
 /** @param {NS} ns */
 export async function main(ns) {
-	const scriptName = 'hack-remote.js';
-	const serverList = await scan(ns);
-	const purchasedServers = serverList.filter(name => name.startsWith('neighbor-'));
-	
-	for (let server of purchasedServers) {
-		const scpStatus = ns.scp(scriptName, server, 'home');
-		if (!scpStatus) {
-			ns.tprint('Failed to copy ' + scriptName + ' on ' + server);
-		}
+    const scriptName = 'hack-remote.js';
+    const serverList = await scan(ns);
+    const purchasedServers = serverList.filter(name => name.startsWith('neighbor-'));
 
-		ns.killall(server);
+    for (let server of purchasedServers) {
+        const scpStatus = ns.scp(scriptName, server, 'home');
+        if (!scpStatus) {
+            ns.tprint('Failed to copy ' + scriptName + ' on ' + server);
+        }
 
-		const maxRam = ns.getServerMaxRam(server);
-		const usedRam = ns.getServerUsedRam(server);
-		const availableRam = maxRam - usedRam;
-		const scriptRam = ns.getScriptRam(scriptName, server);
-		const numThreads = Math.floor(availableRam/scriptRam);
+        ns.killall(server);
 
-		if (numThreads>0) {
-			ns.tprint(`Launching ${scriptName} on ${server} with ${numThreads} threads`);
-			ns.exec(scriptName, server, numThreads);
-		}
-	}
+        const maxRam = ns.getServerMaxRam(server);
+        const usedRam = ns.getServerUsedRam(server);
+        const availableRam = maxRam - usedRam;
+        const scriptRam = ns.getScriptRam(scriptName, server);
+        const numThreads = Math.floor(availableRam/scriptRam);
+
+        if (numThreads>0) {
+            ns.tprint(`Launching ${scriptName} on ${server} with ${numThreads} threads`);
+            ns.exec(scriptName, server, numThreads);
+        }
+    }
 }
