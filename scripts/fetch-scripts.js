2856d5e5379a7fb31b832547cb5534f77b58a917
Antoine Dubé <antoine.dube@gmail.com>
2023-12-14 14:44:03 -0500

    Scripts export

--- scripts/fetch-scripts.js
+++ scripts/fetch-scripts.js
@@ -1,39 +1,39 @@
 /** @param {NS} ns */
 export async function main(ns) {
-	if (ns.getHostname() !== "home") {
-		throw new Exception("Run the script from home");
-	}
+    if (ns.getHostname() !== "home") {
+        throw new Exception("Run the script from home");
+    }
 
-	const scripts = [
-		'bootstrap.js',
-		'buy-darkweb-programs.js',
-		'buy-hacknet-nodes.js',
-		'buy-servers.js',
-		'delete-servers.js',
-		'fetch-scripts.js',
-		'hack-remote.js',
-		'hack-server.js',
-		'hacking-programs.js',
-		'launch-hacking.js',
-		'list-player-karma.js',
-		'list-server-money.js',
-		'list-server-prices.js',
-		'list-server-security-level.js',
-		'manage-bladeburner.js',
-		'manage-corporation.js',
-		'manage-gang.js',
-		'manage-sleeves.js',
-		'scan.js',
-		'spend-hashes.js'
-	];
+    const scripts = [
+        'bootstrap.js',
+        'buy-darkweb-programs.js',
+        'buy-hacknet-servers.js',
+        'buy-servers.js',
+        'delete-servers.js',
+        'fetch-scripts.js',
+        'hack-remote.js',
+        'hack-server.js',
+        'hacking-programs.js',
+        'launch-hacking.js',
+        'list-player-karma.js',
+        'list-server-money.js',
+        'list-server-prices.js',
+        'list-server-security-level.js',
+        'manage-bladeburner.js',
+        'manage-corporation.js',
+        'manage-gang.js',
+        'manage-sleeves.js',
+        'scan.js',
+        'spend-hashes.js'
+    ];
 
-	for (const scriptName of scripts) {
-		ns.tprint(`Fetching: ${scriptName}`);
-		await ns.wget(
-			`https://raw.githubusercontent.com/antoinedube/bitburner-scripts/main/scripts/${scriptName}`,
-			`${scriptName}`
-		);
+    for (const scriptName of scripts) {
+        ns.tprint(`Fetching: ${scriptName}`);
+        await ns.wget(
+            `https://raw.githubusercontent.com/antoinedube/bitburner-scripts/main/scripts/${scriptName}`,
+            `${scriptName}`
+        );
 
-		await ns.sleep(250);
-	}
+        await ns.sleep(250);
+    }
 }
--- scripts/spend-hashes.js
+++ scripts/spend-hashes.js
@@ -26,34 +26,34 @@ export async function main(ns) {
     ns.disableLog('ALL');
 
     /*
-        const upgrades = ns.hacknet.getHashUpgrades();
+            const upgrades = ns.hacknet.getHashUpgrades();
 
-        [
-                "Sell for Money",
-                "Sell for Corporation Funds",
-                "Reduce Minimum Security",
-                "Increase Maximum Money",
-                "Improve Studying",
-                "Improve Gym Training",
-                "Exchange for Corporation Research",
-                "Exchange for Bladeburner Rank",
-                "Exchange for Bladeburner SP",
-                "Generate Coding Contract",
-                "Company Favor"
-        ]
+            [
+                            "Sell for Money",
+                            "Sell for Corporation Funds",
+                            "Reduce Minimum Security",
+                            "Increase Maximum Money",
+                            "Improve Studying",
+                            "Improve Gym Training",
+                            "Exchange for Corporation Research",
+                            "Exchange for Bladeburner Rank",
+                            "Exchange for Bladeburner SP",
+                            "Generate Coding Contract",
+                            "Company Favor"
+            ]
     */
 
     const ten_trillions = 10 * 1000 * 1000 * 1000 * 1000;  // k -> m -> g -> t
     const low_high_hack_exp_limit = 1500;
 
-    for (let i = 0; i < 100; i++) {
-        ns.print(`--> Improve studying ${i} of 100`);
+    for (let i = 0; i < 10; i++) {
+        ns.print(`--> Improve studying ${i} of 10`);
         await spendHashesOnAction(ns, 'Improve Studying', 'home', 1);
     }
 
     while (true) {
         const r = Math.random();
-        if (r < 0.2) {
+        if (r < 0.1) {
             const target = selectRandomServer(ns);
             const minLevel = ns.getServerMinSecurityLevel(target);
             if (minLevel > 1.0 && ns.getHackingLevel() > low_high_hack_exp_limit) {
@@ -61,7 +61,7 @@ export async function main(ns) {
                 const minLevelAfter = ns.getServerMinSecurityLevel(target);
                 ns.print(`Reduced minimum security level on ${target} from ${minLevel} to ${minLevelAfter}`);
             }
-        } else if (r < 0.4) {
+        } else if (r < 0.2) {
             const target = selectRandomServer(ns);
             const maxMoney = ns.getServerMaxMoney(target);
             if (maxMoney < ten_trillions && ns.getHackingLevel() > low_high_hack_exp_limit) {
@@ -69,18 +69,18 @@ export async function main(ns) {
                 const maxMoneyAfter = ns.getServerMaxMoney(target);
                 ns.print(`Increased maximum money on ${target} from ${ns.formatNumber(maxMoney)}\$ to ${ns.formatNumber(maxMoneyAfter)}\$`);
             }
-        /*
-        } else if (r < 0.3) {
-            if (ns.bladeburner.inBladeburner()) {
-                await spendHashesOnAction(ns, 'Exchange for Bladeburner Rank', 'home', 1);
-            }
-        } else if (r < 0.4) {
-            if (ns.bladeburner.inBladeburner()) {
-                await spendHashesOnAction(ns, 'Exchange for Bladeburner SP', 'home', 1);
-            }
-        */
+            /*
+            } else if (r < 0.3) {
+                    if (ns.bladeburner.inBladeburner()) {
+                            await spendHashesOnAction(ns, 'Exchange for Bladeburner Rank', 'home', 1);
+                    }
+            } else if (r < 0.4) {
+                    if (ns.bladeburner.inBladeburner()) {
+                            await spendHashesOnAction(ns, 'Exchange for Bladeburner SP', 'home', 1);
+                    }
+            */
         } else {
-            await spendHashesOnAction(ns, "Sell for Money", "target", 2500);
+            await spendHashesOnAction(ns, "Sell for Money", "target", 25);
             ns.print(`Sold hashes for money`);
         }
 
