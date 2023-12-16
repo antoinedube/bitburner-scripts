e0e802d469ffc0a2a53c76e63e45659782021417
Antoine Dubé <antoine.dube@gmail.com>
2023-12-16 07:54:37 -0500

    Scripts exports. Keep track of saves exports

--- scripts/buy-servers.js
+++ scripts/buy-servers.js
@@ -1,123 +1,123 @@
 import { scanAllNetwork } from "./scan";
 
 function generateUUID() {
-    // https://stackoverflow.com/questions/105034/how-do-i-create-a-guid-uuid
-    // crypto.randomUUID();
-    // crypto.getRandomValues();
+  // https://stackoverflow.com/questions/105034/how-do-i-create-a-guid-uuid
+  // crypto.randomUUID();
+  // crypto.getRandomValues();
 }
 
 /** @param {NS} ns */
 function launchScript(ns, scriptName, server) {
-    const scpStatus = ns.scp(scriptName, server, 'home');
-    if (!scpStatus) {
-        ns.print('Failed to copy ' + scriptName + ' on ' + server);
-    }
+  const scpStatus = ns.scp(scriptName, server, 'home');
+  if (!scpStatus) {
+    ns.print('Failed to copy ' + scriptName + ' on ' + server);
+  }
 
-    ns.killall(server);
+  ns.killall(server);
 
-    const maxRam = ns.getServerMaxRam(server);
-    const usedRam = ns.getServerUsedRam(server);
-    const availableRam = maxRam - usedRam;
-    const scriptRam = ns.getScriptRam(scriptName, server);
-    const numThreads = Math.floor(availableRam / scriptRam);
-    if (numThreads > 0) {
-        if (ns.exec(scriptName, server, numThreads) == 0) {
-            ns.print('Error launching script');
-        }
+  const maxRam = ns.getServerMaxRam(server);
+  const usedRam = ns.getServerUsedRam(server);
+  const availableRam = maxRam - usedRam;
+  const scriptRam = ns.getScriptRam(scriptName, server);
+  const numThreads = Math.floor(availableRam / scriptRam);
+  if (numThreads > 0) {
+    if (ns.exec(scriptName, server, numThreads) == 0) {
+      ns.print('Error launching script');
     }
+  }
 }
 
 /** @param {NS} ns */
 export async function main(ns) {
-    ns.disableLog('ALL');
-    const BUYING_DELAY = 250;
-    const UPGRADING_DELAY = 5 * 1000;
-    const FOLLOWING_BATCH_DELAY = 1000*60*5;
-    const HOME_SERVER = 'home';
+  ns.disableLog('ALL');
+  const BUYING_DELAY = 250;
+  const UPGRADING_DELAY = 5 * 1000;
+  const FOLLOWING_BATCH_DELAY = 1000 * 60 * 5;
+  const HOME_SERVER = 'home';
 
-    let targetRam = 4;
-    while (targetRam <= ns.getPurchasedServerMaxRam()) {
-        const maxNumberOfServers = ns.getPurchasedServerLimit();
-        const availableMoney = ns.getServerMoneyAvailable('home');
-        const serverCost = ns.getPurchasedServerCost(targetRam);
+  let targetRam = 4;
+  while (targetRam <= ns.getPurchasedServerMaxRam()) {
+    const maxNumberOfServers = ns.getPurchasedServerLimit();
+    const availableMoney = ns.getServerMoneyAvailable('home');
+    const serverCost = ns.getPurchasedServerCost(targetRam);
 
-        if (availableMoney < maxNumberOfServers * serverCost) {
-            targetRam /= 2;
-            break;
-        }
-
-        targetRam *= 2;
+    if (availableMoney < maxNumberOfServers * serverCost) {
+      targetRam /= 2;
+      break;
     }
-
-    if (targetRam < 8) {
-        targetRam = 8;
-    }
-
-    ns.print(`Starting target ram: ${targetRam}`);
-
-    // Purchase missing servers
-    while (true) {
-        // List current servers
-        const serverList = scanAllNetwork(ns);
-        let purchasedServers = serverList.filter(name => name.startsWith('neighbor-'));
-
-        // Stopping criteria
-        if (purchasedServers.length == ns.getPurchasedServerLimit()) {
-            break;
-        }
-
-        // If limit is not reached, buy server at current targetRam
-        if (ns.getPurchasedServerCost(targetRam) < ns.getServerMoneyAvailable(HOME_SERVER)) {
-            const name = `neighbor-${purchasedServers.length}`;
-            ns.print(`Purchasing server ${name}`);
-            ns.purchaseServer(name, targetRam);
-            purchasedServers.push(name);
-        }
-
-        await ns.sleep(BUYING_DELAY);
-    }
-
-    const purchasedServers = scanAllNetwork(ns).filter(name => name.startsWith('neighbor-'));
-    ns.print(`Purchased servers: ${purchasedServers}`);
 
     targetRam *= 2;
-    while (true) {
-        // Stopping criteria
-        let countServerWithTargetRam = 0;
-        for (const purchasedServer of purchasedServers) {
-            const purchasedServerRam = ns.getServer(purchasedServer).maxRam;
-            if (purchasedServerRam >= targetRam) {
-                countServerWithTargetRam++;
-            }
-        }
+  }
 
-        ns.print(`Number of servers at target (target: ${ns.formatRam(targetRam)} / max: ${ns.formatRam(ns.getPurchasedServerMaxRam())}): ${countServerWithTargetRam} vs ${ns.getPurchasedServerLimit()}`);
-        if (countServerWithTargetRam == ns.getPurchasedServerLimit()) {
-            if (targetRam >= ns.getPurchasedServerMaxRam()) {
-                break;
-            }
+  if (targetRam < 8) {
+    targetRam = 8;
+  }
 
-            targetRam *= 2;
+  ns.print(`Starting target ram: ${targetRam}`);
 
-            await ns.sleep(FOLLOWING_BATCH_DELAY);
-        }
+  // Purchase missing servers
+  while (true) {
+    // List current servers
+    const serverList = scanAllNetwork(ns);
+    let purchasedServers = serverList.filter(name => name.startsWith('neighbor-'));
 
-        for (const purchasedServer of purchasedServers) {
-            const purchasedServerRam = ns.getServer(purchasedServer).maxRam;
-            if (purchasedServerRam < targetRam) {
-                const moneyAvailable = ns.getServerMoneyAvailable('home');
-                const upgradeCost = ns.getPurchasedServerUpgradeCost(purchasedServer, targetRam);
-                if (upgradeCost < moneyAvailable) {
-                    if (ns.upgradePurchasedServer(purchasedServer, targetRam)) {
-                        ns.print(`Upgraded ${purchasedServer} to ${targetRam}Gb with cost of ${ns.formatNumber(upgradeCost)}\$`);
-                        launchScript(ns, 'hack-remote.js', purchasedServer);
-                    } else {
-                        ns.print(`Error while upgrading purchased server ${purchasedServer} to ${targetRam}Gb`);
-                    }
-                }
-            }
-        }
-
-        await ns.sleep(UPGRADING_DELAY);
+    // Stopping criteria
+    if (purchasedServers.length == ns.getPurchasedServerLimit()) {
+      break;
     }
+
+    // If limit is not reached, buy server at current targetRam
+    if (ns.getPurchasedServerCost(targetRam) < ns.getServerMoneyAvailable(HOME_SERVER)) {
+      const name = `neighbor-${purchasedServers.length}`;
+      ns.print(`Purchasing server ${name}`);
+      ns.purchaseServer(name, targetRam);
+      purchasedServers.push(name);
+    }
+
+    await ns.sleep(BUYING_DELAY);
+  }
+
+  const purchasedServers = scanAllNetwork(ns).filter(name => name.startsWith('neighbor-'));
+  ns.print(`Purchased servers: ${purchasedServers}`);
+
+  targetRam *= 2;
+  while (true) {
+    // Stopping criteria
+    let countServerWithTargetRam = 0;
+    for (const purchasedServer of purchasedServers) {
+      const purchasedServerRam = ns.getServer(purchasedServer).maxRam;
+      if (purchasedServerRam >= targetRam) {
+        countServerWithTargetRam++;
+      }
+    }
+
+    ns.print(`Number of servers at target (target: ${ns.formatRam(targetRam)} / max: ${ns.formatRam(ns.getPurchasedServerMaxRam())}): ${countServerWithTargetRam} vs ${ns.getPurchasedServerLimit()}`);
+    if (countServerWithTargetRam == ns.getPurchasedServerLimit()) {
+      if (targetRam >= ns.getPurchasedServerMaxRam()) {
+        break;
+      }
+
+      targetRam *= 2;
+
+      await ns.sleep(FOLLOWING_BATCH_DELAY);
+    }
+
+    for (const purchasedServer of purchasedServers) {
+      const purchasedServerRam = ns.getServer(purchasedServer).maxRam;
+      if (purchasedServerRam < targetRam) {
+        const moneyAvailable = ns.getServerMoneyAvailable('home');
+        const upgradeCost = ns.getPurchasedServerUpgradeCost(purchasedServer, targetRam);
+        if (upgradeCost < moneyAvailable) {
+          if (ns.upgradePurchasedServer(purchasedServer, targetRam)) {
+            ns.print(`Upgraded ${purchasedServer} to ${targetRam}Gb with cost of ${ns.formatNumber(upgradeCost)}\$`);
+            launchScript(ns, 'hack-remote.js', purchasedServer);
+          } else {
+            ns.print(`Error while upgrading purchased server ${purchasedServer} to ${targetRam}Gb`);
+          }
+        }
+      }
+    }
+
+    await ns.sleep(UPGRADING_DELAY);
+  }
 }
--- scripts/spend-hashes.js
+++ scripts/spend-hashes.js
@@ -2,88 +2,88 @@ import { scanAllNetwork } from "./scan.js";
 
 /** @param {NS} ns */
 async function spendHashesOnAction(ns, action, target, amount) {
-    while (ns.hacknet.numHashes() < ns.hacknet.hashCost(action, amount)) {
-        await ns.sleep(1000);
-    }
+  while (ns.hacknet.numHashes() < ns.hacknet.hashCost(action, amount)) {
+    await ns.sleep(1000);
+  }
 
-    if (!ns.hacknet.spendHashes(action, target, amount)) {
-        ns.print(`Error while executing ${action} on ${target} with n=${amount}`);
-    }
+  if (!ns.hacknet.spendHashes(action, target, amount)) {
+    ns.print(`Error while executing ${action} on ${target} with n=${amount}`);
+  }
 }
 
 /** @param {NS} ns */
 function selectRandomServer(ns) {
-    const serversToAvoid = ['CSEC', 'I.I.I.I', 'run4theh111z', 'avmnite-02h', 'The-Cave', 'w0r1d_d43m0n'];
+  const serversToAvoid = ['CSEC', 'I.I.I.I', 'run4theh111z', 'avmnite-02h', 'The-Cave', 'w0r1d_d43m0n'];
 
-    const fullServerList = scanAllNetwork(ns, 'home');
-    const filteredServerList = fullServerList.filter(name => !name.startsWith('neighbor-') && !name.startsWith('hacknet-') && !serversToAvoid.includes(name));
-    const serverIndex = Math.floor(Math.random() * filteredServerList.length);
-    return filteredServerList[serverIndex];
+  const fullServerList = scanAllNetwork(ns, 'home');
+  const filteredServerList = fullServerList.filter(name => !name.startsWith('neighbor-') && !name.startsWith('hacknet-') && !serversToAvoid.includes(name));
+  const serverIndex = Math.floor(Math.random() * filteredServerList.length);
+  return filteredServerList[serverIndex];
 }
 
 /** @param {NS} ns */
 export async function main(ns) {
-    ns.disableLog('ALL');
+  ns.disableLog('ALL');
 
-    /*
-            const upgrades = ns.hacknet.getHashUpgrades();
+  /*
+          const upgrades = ns.hacknet.getHashUpgrades();
 
-            [
-                            "Sell for Money",
-                            "Sell for Corporation Funds",
-                            "Reduce Minimum Security",
-                            "Increase Maximum Money",
-                            "Improve Studying",
-                            "Improve Gym Training",
-                            "Exchange for Corporation Research",
-                            "Exchange for Bladeburner Rank",
-                            "Exchange for Bladeburner SP",
-                            "Generate Coding Contract",
-                            "Company Favor"
-            ]
-    */
+          [
+                          "Sell for Money",
+                          "Sell for Corporation Funds",
+                          "Reduce Minimum Security",
+                          "Increase Maximum Money",
+                          "Improve Studying",
+                          "Improve Gym Training",
+                          "Exchange for Corporation Research",
+                          "Exchange for Bladeburner Rank",
+                          "Exchange for Bladeburner SP",
+                          "Generate Coding Contract",
+                          "Company Favor"
+          ]
+  */
 
-    const ten_trillions = 10 * 1000 * 1000 * 1000 * 1000;  // k -> m -> g -> t
-    const low_high_hack_exp_limit = 1500;
+  const ten_trillions = 10 * 1000 * 1000 * 1000 * 1000;  // k -> m -> g -> t
+  const low_high_hack_exp_limit = 1500;
 
-    for (let i = 0; i < 10; i++) {
-        ns.print(`--> Improve studying ${i} of 10`);
-        await spendHashesOnAction(ns, 'Improve Studying', 'home', 1);
+  for (let i = 0; i < 5; i++) {
+    ns.print(`--> Improve studying ${i} of 5`);
+    await spendHashesOnAction(ns, 'Improve Studying', 'home', 1);
+  }
+
+  while (true) {
+    const r = Math.random();
+    if (r < 0.1) {
+      const target = selectRandomServer(ns);
+      const minLevel = ns.getServerMinSecurityLevel(target);
+      if (minLevel > 1.0 && ns.getHackingLevel() > low_high_hack_exp_limit) {
+        await spendHashesOnAction(ns, "Reduce Minimum Security", target, 1);
+        const minLevelAfter = ns.getServerMinSecurityLevel(target);
+        ns.print(`Reduced minimum security level on ${target} from ${minLevel} to ${minLevelAfter}`);
+      }
+    } else if (r < 0.2) {
+      const target = selectRandomServer(ns);
+      const maxMoney = ns.getServerMaxMoney(target);
+      if (maxMoney < ten_trillions && ns.getHackingLevel() > low_high_hack_exp_limit) {
+        await spendHashesOnAction(ns, "Increase Maximum Money", target, 1);
+        const maxMoneyAfter = ns.getServerMaxMoney(target);
+        ns.print(`Increased maximum money on ${target} from ${ns.formatNumber(maxMoney)}\$ to ${ns.formatNumber(maxMoneyAfter)}\$`);
+      }
+      /*
+      } else if (r < 0.3) {
+              if (ns.bladeburner.inBladeburner()) {
+                      await spendHashesOnAction(ns, 'Exchange for Bladeburner Rank', 'home', 1);
+              }
+      } else if (r < 0.4) {
+              if (ns.bladeburner.inBladeburner()) {
+                      await spendHashesOnAction(ns, 'Exchange for Bladeburner SP', 'home', 1);
+              }
+      */
+    } else {
+      await spendHashesOnAction(ns, "Sell for Money", "target", 25);
+      ns.print(`Sold hashes for money`);
     }
 
-    while (true) {
-        const r = Math.random();
-        if (r < 0.1) {
-            const target = selectRandomServer(ns);
-            const minLevel = ns.getServerMinSecurityLevel(target);
-            if (minLevel > 1.0 && ns.getHackingLevel() > low_high_hack_exp_limit) {
-                await spendHashesOnAction(ns, "Reduce Minimum Security", target, 1);
-                const minLevelAfter = ns.getServerMinSecurityLevel(target);
-                ns.print(`Reduced minimum security level on ${target} from ${minLevel} to ${minLevelAfter}`);
-            }
-        } else if (r < 0.2) {
-            const target = selectRandomServer(ns);
-            const maxMoney = ns.getServerMaxMoney(target);
-            if (maxMoney < ten_trillions && ns.getHackingLevel() > low_high_hack_exp_limit) {
-                await spendHashesOnAction(ns, "Increase Maximum Money", target, 1);
-                const maxMoneyAfter = ns.getServerMaxMoney(target);
-                ns.print(`Increased maximum money on ${target} from ${ns.formatNumber(maxMoney)}\$ to ${ns.formatNumber(maxMoneyAfter)}\$`);
-            }
-            /*
-            } else if (r < 0.3) {
-                    if (ns.bladeburner.inBladeburner()) {
-                            await spendHashesOnAction(ns, 'Exchange for Bladeburner Rank', 'home', 1);
-                    }
-            } else if (r < 0.4) {
-                    if (ns.bladeburner.inBladeburner()) {
-                            await spendHashesOnAction(ns, 'Exchange for Bladeburner SP', 'home', 1);
-                    }
-            */
-        } else {
-            await spendHashesOnAction(ns, "Sell for Money", "target", 25);
-            ns.print(`Sold hashes for money`);
-        }
-
-        await ns.sleep(250);
-    }
+    await ns.sleep(250);
+  }
 }
